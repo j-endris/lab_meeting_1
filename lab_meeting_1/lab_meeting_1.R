87a7a2fd127952bf22c6971dd1f0208f2909b7d6
Joe Endris <joseph.endris@outlook.com>
2022-02-26 09:50:28 -0600

    ggplot for min/max temp by date

--- lab_meeting_1/lab_meeting_1.R
+++ lab_meeting_1/lab_meeting_1.R
@@ -1,2 +1,24 @@
+#lab_meeting_assignment
+#Joe Endris
+
 library(dplyr)
 library(ggplot2)
+
+
+climate <-read.csv("clarksville_climate.csv")
+
+glimpse(climate)
+head(climate)
+
+#scatterplot for temperature max per day
+ggplot(climate)+
+  geom_point(aes(x=DATE, y=TMAX),color=as.factor(), size=1)
+  theme_bw()+
+  xlab("Date")+
+  ylab("Temperature")+
+    ggtitle("Temperature Minimum and Maximum by Date")
+
+  
+monthly_mean <- montly_mean %>%
+  group_by(month) %>%
+  summarize(mean_temp = mean(TMAX))
