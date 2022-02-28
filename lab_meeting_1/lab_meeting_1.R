38103c24604340e2c03a9693abb7f92812faeca3
Joe Endris <joseph.endris@outlook.com>
2022-02-28 08:26:02 -0600

    Finished raw code, need to adjust graph outputs for months in chronological order instead of alphabet

--- lab_meeting_1/lab_meeting_1.R
+++ lab_meeting_1/lab_meeting_1.R
@@ -3,22 +3,80 @@
 
 library(dplyr)
 library(ggplot2)
+library(gridExtra)
 
+raw_climate <-read.csv("clarksville_climate.csv")
 
-climate <-read.csv("clarksville_climate.csv")
+#clean shop, happy shop
+clean_TMAX <- filter(raw_climate,TMAX !="")
+clean_TMIN <- filter(clean_TMAX,TMIN !="")
+climate <- filter(clean_TMIN, YEAR !="")
 
 glimpse(climate)
 head(climate)
 
-#scatterplot for temperature max per day
-ggplot(climate)+
-  geom_point(aes(x=DATE, y=TMAX),color=as.factor(), size=1)
+#create mean TMIN/TMAX by month
+monthlyTMAX <- climate %>%
+  group_by(MONTH) %>%
+  summarise(meanTMAX = mean(TMAX),
+            sdTMAX = sd(TMAX))
+
+monthlyTMIN <- climate %>%
+  group_by(MONTH) %>%
+  summarise(
+    meanTMIN = mean(TMIN),
+            sdTMIN = sd(TMIN))
+
+
+#plot monthly mean TMAX/TMIN
+monthlyTMAX_plot <- ggplot(monthlyTMAX, aes(x= MONTH, y=meanTMAX))+
+  geom_point(size=2, color="red")+
+  xlab("Month")+
+  ylab("Temperature")+
+  ggtitle("Monthly Mean Maximum Temperature")
+
+monthlyTMIN_plot <- ggplot(monthlyTMIN, aes(x= MONTH, y=meanTMIN))+
+  geom_point(size=2, color="blue")+
+  xlab("Month")+
+  ylab("Temperature")+
+  ggtitle("Monthly Mean Minimum Temperature")
+
+grid.arrange(monthlyTMAX_plot, monthlyTMIN_plot)
+
+#create mean TMIN/TMAX by year
+annualTMAX <-climate %>%
+  group_by(YEAR) %>%
+  summarise(meanTMAX = mean(TMAX),
+            sdTMAX = sd(TMAX))
+
+annualTMIN <-climate %>%
+  group_by(YEAR) %>%
+  summarise(
+    meanTMIN = mean(TMIN),
+    sdTMIN = sd(TMIN))
+
+
+#plot annual mean TMAX/TMIN
+annualTMAX_plot <- ggplot(annualTMAX, aes(x= YEAR, y=meanTMAX))+
+  geom_point(size=2, color="red")+
+  xlab("Year")+
+  ylab("Temperature")+
+  ggtitle("Annual Mean Maximum Temperature by Year")
+
+annualTMIN_plot <- ggplot(annualTMIN, aes(x= YEAR, y=meanTMIN))+
+  geom_point(size=2, color="blue")+
+  xlab("Year")+
+  ylab("Temperature")+
+  ggtitle("Annual Mean Minimum Temperature by Year")
+
+grid.arrange(annualTMAX_plot, annualTMIN_plot)
+
+#ggplot for temperature max per day
+ggplot(climate, aes(x=DATE))+
+  geom_point(aes(y=TMAX), color="red", size=1)+
+  geom_point(aes(y=TMIN), color="blue", size=1)+
   theme_bw()+
   xlab("Date")+
   ylab("Temperature")+
-    ggtitle("Temperature Minimum and Maximum by Date")
+  ggtitle("Temperature Minimum and Maximum by Date")
 
-  
-monthly_mean <- montly_mean %>%
-  group_by(month) %>%
-  summarize(mean_temp = mean(TMAX))
